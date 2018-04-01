d3d355c0544cd119e9c4ae645c00811139f42e80
Scott McKell <scott.g.mckell@gmail.com>
2018-03-31 18:09:30 -0600

    fixed up the code to be object-based in a different manner cmt#2

--- README.md
+++ README.md
@@ -1,8 +1,7 @@
 This is a ruby-based, console running game of Tic-Tac-Toe.
 
-Open a terminal in the containing directory and(if you have ruby installed) type "irb tictactoe.rb" to play!
+Open a terminal in the containing directory and(if you have ruby installed) type "ruby tictactoe.rb" to play!
 
 NOTES:
-Maybe switching the board for an object and the spaces for variables will shorten the code.
-
-Seems like it could be utilized with a GUI of some sort in the future, rather than in the command line.
+Updated to work as a single object now.
+can add savability
--- tictactoe.rb
+++ tictactoe.rb
@@ -1,380 +1,315 @@
-$count = 0
-class Space
-	def initialize(spot,status)
-		@name = spot
-		@status = status
+class Game
+	attr_accessor :a, :b, :c, :d, :e, :f, :g, :h, :i, :string
+	def initialize
+		@a = " "
+		@b = " "
+		@c = " "
+		@d = " "
+		@e = " "
+		@f = " "
+		@g = " "
+		@h = " "
+		@i = " "
+		@count = 0
 	end
-	def name
-		@name
+	def wincheck
+		#Vertical wins
+		if (@a == @d && @d == @g && @a != " ")
+			return true
+		elsif (@b == @e && @e == @h && @h != " ")
+			return true
+		elsif (@c == @f && @f == @i && @i != " ")
+			return true
+		#Horizontal wins
+		elsif (@a == @b && @b == @c && @c != " ")
+			return true
+		elsif (@d == @e && @e == @f && @f != " ")
+			return true
+		elsif (@g == @h && @h == @i && @i != " ")
+			return true
+		#Diagonal wins
+		elsif (@g == @e && @e == @c && @c != " ")
+			return true
+		elsif (@a == @e && @e == @i && @i != " ")
+			return true
+		else
+			return false
+		end
 	end
-	def status
-		@status
-	end
-	@changed = false
-	def changed
-		@changed
-	end
-	def x
-			@status = "x"
-			@changed = true
-			$count += 1
-	end
-	def o
-		@status = "o"
-		@changed = true
-		$count += 1
-	end
-end
-
-$winstate = false
-$tiestate = false
-
-reset = Proc.new do
-	$winstate = false
-	$tiestate = false
-	@a = Space.new("A"," ")
-	@b = Space.new("B"," ")
-	@c = Space.new("C"," ")
-	@d = Space.new("D"," ")
-	@e = Space.new("E"," ")
-	@f = Space.new("F"," ")
-	@g = Space.new("G"," ")
-	@h = Space.new("H"," ")
-	@i = Space.new("I"," ")
-end
-
-board = Proc.new do
-	puts "Tic Tac Toe!
+	def player_one
+		puts "Tic Tac Toe!
 
 
 
 Layout:     |Your Board:
- A | B | C  | #{@a.status} | #{@b.status} | #{@c.status} 
+ A | B | C  | #{@a} | #{@b} | #{@c} 
 ---+---+--- |---+---+---
- D | E | F  | #{@d.status} | #{@e.status} | #{@f.status} 
+ D | E | F  | #{@d} | #{@e} | #{@f} 
 ---+---+--- |---+---+---
- G | H | I  | #{@g.status} | #{@h.status} | #{@i.status} 
+ G | H | I  | #{@g} | #{@h} | #{@i} 
  
 
  "
-end
-
-gameplay_one = Proc.new do
-	board.call
-	puts "Player one (x), your turn! (put the letter you want to mark)"
-	@choice = gets.chomp.downcase
-	case @choice
-	when "a"
-		if @a.status != " "
-			system 'clear'
-			puts "ERROR! please choose a free space."
-			gameplay_one.call
+		puts "Player one (x), your turn! (put the letter you want to mark)"
+		choice = gets.chomp.downcase
+		case choice
+		when "a"
+			if @a != " "
+				system 'clear'
+				puts "ERROR! please choose a free space."
+				player_one
+			else
+				@a = "x"
+			end
+		when "b"
+			if @b != " "
+				system 'clear'
+				puts "ERROR! please choose a free space."
+				player_one
+			else
+				@b = "x"
+			end
+		when "c"
+			if @c != " "
+				system 'clear'
+				puts "ERROR! please choose a free space."
+				player_one
+			else
+				@c = "x"
+			end
+		when "d"
+			if @d != " "
+				system 'clear'
+				puts "ERROR! please choose a free space."
+				player_one
+			else
+				@d = "x"
+			end
+		when "e"
+			if @e != " "
+				system 'clear'
+				puts "ERROR! please choose a free space."
+				player_one
+			else
+				@e = "x"
+			end
+		when "f"
+			if @f != " "
+				system 'clear'
+				puts "ERROR! please choose a free space."
+				player_one
+			else
+				@f = "x"
+			end
+		when "g"
+			if @g != " "
+				system 'clear'
+				puts "ERROR! please choose a free space."
+				player_one
+			else
+				@g = "x"
+			end
+		when "h"
+			if @h != " "
+				system 'clear'
+				puts "ERROR! please choose a free space."
+				player_one
+			else
+				@h = "x"
+			end
+		when "i"
+			if @i != " "
+				system 'clear'
+				puts "ERROR! please choose a free space."
+				player_one
+			else
+				@i = "x"
+			end
 		else
-			@a.x
-		end
-	when "b"
-		if @b.status != " "
 			system 'clear'
-			puts "ERROR! please choose a free space."
-			gameplay_one.call
-		else
-			@b.x
+			puts "ERROR! please put a-i."
+			player_one
 		end
-	when "c"
-		if @c.status != " "
-			system 'clear'
-			puts "ERROR! please choose a free space."
-			gameplay_one.call
-		else
-			@c.x
-		end
-	when "d"
-		if @d.status != " "
-			system 'clear'
-			puts "ERROR! please choose a free space."
-			gameplay_one.call
-		else
-			@d.x
-		end
-	when "e"
-		if @e.status != " "
-			system 'clear'
-			puts "ERROR! please choose a free space."
-			gameplay_one.call
-		else
-			@e.x
-		end
-	when "f"
-		if @f.status != " "
-			system 'clear'
-			puts "ERROR! please choose a free space."
-			gameplay_one.call
-		else
-			@f.x
-		end
-	when "g"
-		if @g.status != " "
-			system 'clear'
-			puts "ERROR! please choose a free space."
-			gameplay_one.call
-		else
-			@g.x
-		end
-	when "h"
-		if @h.status != " "
-			system 'clear'
-			puts "ERROR! please choose a free space."
-			gameplay_one.call
-		else
-			@h.x
-		end
-	when "i"
-		if @i.status != " "
-			system 'clear'
-			puts "ERROR! please choose a free space."
-			gameplay_one.call
-		else
-			@i.x
-		end
-	else
-		system 'clear'
-		puts "ERROR! please put a-i."
-		gameplay_one.call
+		@count += 1
+		system "clear"
 	end
-end
-gameplay_two = Proc.new do
-	board.call
-	puts "Player two (o), your turn! (put the letter you want to mark)"
-	@choice = gets.chomp.downcase
-	case @choice
-	when "a"
-		if @a.status != " "
-			system 'clear'
-			puts "ERROR! Please choose a free space."
-			gameplay_two.call
+
+	def player_two
+		puts "Tic Tac Toe!
+
+
+
+Layout:     |Your Board:
+ A | B | C  | #{@a} | #{@b} | #{@c} 
+---+---+--- |---+---+---
+ D | E | F  | #{@d} | #{@e} | #{@f} 
+---+---+--- |---+---+---
+ G | H | I  | #{@g} | #{@h} | #{@i} 
+ 
+
+ "
+		puts "Player two (o), your turn! (put the letter you want to mark)"
+		choice = gets.chomp.downcase
+		case choice
+		when "a"
+			if @a != " "
+				system 'clear'
+				puts "ERROR! Please choose a free space."
+				player_two
+			else
+				@a = "o" 
+			end
+		when "b"
+			if @b != " "
+				system 'clear'
+				puts "ERROR! Please choose a free space."
+				player_two
+			else
+				@b = "o" 
+			end
+		when "c"
+			if @c != " "
+				system 'clear'
+				puts "ERROR! Please choose a free space."
+				player_two
+			else
+				@c = "o" 
+			end
+		when "d"
+			if @d != " "
+				system 'clear'
+				puts "ERROR! Please choose a free space."
+				player_two
+			else
+				@d = "o" 
+			end
+		when "e" 
+			if @e != " "
+				system 'clear'
+				puts "ERROR! Please choose a free space."
+				player_two
+			else
+				@e = "o" 
+			end
+		when "f"
+			if @f != " "
+				system 'clear'
+				puts "ERROR! Please choose a free space."
+				player_two
+			else
+				@f = "o" 
+			end
+		when "g"
+			if @g != " "
+				system 'clear'
+				puts "ERROR! Please choose a free space."
+				player_two
+			else
+				@g = "o" 
+			end
+		when "h"
+			if @h != " "
+				system 'clear'
+				puts "ERROR! Please choose a free space."
+				player_two
+			else
+				@h = "o" 
+			end
+		when "i"
+			if @i != " "
+				system 'clear'
+				puts "ERROR! Please choose a free space."
+				player_two
+			else
+				@i = "o" 
+			end
 		else
-			@a.o 
-		end
-	when "b"
-		if @b.status != " "
 			system 'clear'
-			puts "ERROR! Please choose a free space."
-			gameplay_two.call
-		else
-			@b.o 
+			puts "ERROR! please put a-i"
+			player_two
 		end
-	when "c"
-		if @c.status != " "
-			system 'clear'
-			puts "ERROR! Please choose a free space."
-			gameplay_two.call
-		else
-			@c.o 
-		end
-	when "d"
-		if @d.status != " "
-			system 'clear'
-			puts "ERROR! Please choose a free space."
-			gameplay_two.call
-		else
-			@d.o 
-		end
-	when "e" 
-		if @e.status != " "
-			system 'clear'
-			puts "ERROR! Please choose a free space."
-			gameplay_two.call
-		else
-			@e.o 
-		end
-	when "f"
-		if @f.status != " "
-			system 'clear'
-			puts "ERROR! Please choose a free space."
-			gameplay_two.call
-		else
-			@f.o 
-		end
-	when "g"
-		if @g.status != " "
-			system 'clear'
-			puts "ERROR! Please choose a free space."
-			gameplay_two.call
-		else
-			@g.o 
-		end
-	when "h"
-		if @h.status != " "
-			system 'clear'
-			puts "ERROR! Please choose a free space."
-			gameplay_two.call
-		else
-			@h.o 
-		end
-	when "i"
-		if @i.status != " "
-			system 'clear'
-			puts "ERROR! Please choose a free space."
-			gameplay_two.call
-		else
-			@i.o 
-		end
-	else
-		system 'clear'
-		puts "ERROR! please put a-i"
-		gameplay_two.call
+		@count += 1
+		system "clear"
 	end
-end
 
-wincheck = Proc.new do
-	#Vertical wins
-	if (@a.status == @d.status && @d.status == @g.status && @a.changed)
-		$winstate = true
-	elsif (@b.status == @e.status && @e.status == @h.status && @h.changed)
-		$winstate = true
-	elsif (@c.status == @f.status && @f.status == @i.status && @i.changed)
-		$winstate = true
-	#Horizontal wins
-	elsif (@a.status == @b.status && @b.status == @c.status && @c.changed)
-		$winstate = true
-	elsif (@d.status == @e.status && @e.status == @f.status && @f.changed)
-		$winstate = true
-	elsif (@g.status == @h.status && @h.status == @i.status && @i.changed)
-		$winstate = true
-	#Diagonal wins
-	elsif (@g.status == @e.status && @e.status == @c.status && @c.changed)
-		$winstate = true
-	elsif (@a.status == @e.status && @e.status == @i.status && @i.changed)
-		$winstate = true
-	end
-end
+	def win?
+		if wincheck
+			puts "Tic Tac Toe!
 
-tiecheck = Proc.new do
-	#Only two ties possible
-	if (@d.status == @h.status && @d.status == @c.status && @c.changed)
-		$tiestate = true
-	elsif (@h.status == @f.status && @f.status == @a.status && @a.changed)
-		$tiestate = true
-	end
-end
 
-start = Proc.new do
 
-	until $count === 10
-		system 'clear'
-		gameplay_one.call
-		wincheck.call
-		if $winstate
-			x = false
-			system 'clear'
-			until x == true
-				puts "
+Layout:     |Your Board:
+ A | B | C  | #{@a} | #{@b} | #{@c} 
+---+---+--- |---+---+---
+ D | E | F  | #{@d} | #{@e} | #{@f} 
+---+---+--- |---+---+---
+ G | H | I  | #{@g} | #{@h} | #{@i} 
+ 
+
+ "
+			puts "
 
 **************************************************
                     YOU WIN!!!!
 **************************************************
 
 "
-				puts "Play again? (y/n)"
-				@again = gets.chomp.downcase
-				if @again == "y"
-					x = true
-					reset.call
-					start.call
-				elsif @again == "n"
-					x = true
-					exit
-				else
-					system 'clear'
-					puts "ERROR! You must put y or n."
-				end
+			puts "Play again? (y/n)"
+			again = gets.chomp.downcase
+			if again == "y"
+				load "tictactoe.rb"
+			elsif again == "n"
+				system "clear"
+				exit
+			else
+				system "clear"
+				puts "ERROR! Please put y or n" 
+				win?
 			end
-		end
-		tiecheck.call
-		if $tiestate
-			y = false
-			system 'clear'
-			until y == true
-				puts "
+		elsif @count == 9
+			puts "Tic Tac Toe!
 
-*************************************************
-                 CAT'S GAME! (Tie)
-*************************************************
 
-"
-				puts "Play again? (y/n)"
-				@again = gets.chomp.downcase
-				if @again == "y"
-					y = true
-					reset.call
-					start.call
-				elsif @again == "n"
-					y = true
-					exit
-				else
-					system 'clear'
-					puts "ERROR! You must put y or n."
-				end
-			end
-		end
-		system 'clear'
-		gameplay_two.call
-		wincheck.call
-		if $winstate
-			x = false
-			system 'clear'
-			until x == true
-				puts "
+
+Layout:     |Your Board:
+ A | B | C  | #{@a} | #{@b} | #{@c} 
+---+---+--- |---+---+---
+ D | E | F  | #{@d} | #{@e} | #{@f} 
+---+---+--- |---+---+---
+ G | H | I  | #{@g} | #{@h} | #{@i} 
+ 
+
+ "
+			puts "
 
 **************************************************
-                    YOU WIN!!!!
+                CAT'S GAME! (tie)
 **************************************************
 
-"
-				puts "Play again? (y/n)"
-				@again = gets.chomp.downcase
-				if @again == "y"
-					x = true
-					reset.call
-					start.call
-				elsif @again == "n"
-					x = true
-					exit
-				else
-					system 'clear'
-					puts "ERROR! You must put y or n."
-				end
+			"
+			puts "Play again? (y/n)"
+			again = gets.chomp.downcase
+			if again == "y"
+				load "tictactoe.rb"
+			elsif again == "n"
+				system "clear"
+				exit
+			else
+				system "clear"
+				puts "ERROR! Please put y or n" 
+				win?
 			end
-		end
-		tiecheck.call
-		if $tiestate
-			y = false
-			system 'clear'
-			until y == true
-				puts "
-
-*************************************************
-                 CAT'S GAME! (Tie)
-*************************************************
-
-"
-				puts "Play again? (y/n)"
-				@again = gets.chomp.downcase
-				if @again == "y"
-					y = true
-					reset.call
-					start.call
-				elsif @again == "n"
-					y = true
-					exit
-				else
-					system 'clear'
-					puts "ERROR! You must put y or n."
-				end
-			end
+		else
+			return nil
 		end
 	end
 end
 
-reset.call
-start.call
+game = Game.new
+system "clear"
+while 1
+	game.player_one
+	game.win?
+	game.player_two
+	game.win?
+end
